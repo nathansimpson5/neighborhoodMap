0d031beb64aa14473a7476390f67ea8e69d72069
Nathan Simpson <nathansimpson5@gmail.com>
2017-08-02 15:42:37 -0400

    add infowindow

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 function initMap() {
-	var uluru = {lat: -25.363, lng: 131.044};
-	
+	var midtown = {lat: 33.7861132, lng: -84.3896419};
+
+	//make map and center it on Atlantic House
 	var map = new google.maps.Map(document.getElementById('map'), {
-	  zoom: 4,
-	  center: uluru
+	  zoom: 15,
+	  center: midtown
 	});
 
+	// Create market for midtown
 	var marker = new google.maps.Marker({
-	  position: uluru,
+	  position: midtown,
 	  map: map
 	});
+
+	// Click handler to make infowindow appear
+	marker.addListener('click', function() {
+		infowindow.open(map,marker);
+	});
+
+	// Add infowindow
+	var infowindow = new google.maps.InfoWindow({
+		content: "Nathan House"
+	});
+
+
+
+
+
 }
