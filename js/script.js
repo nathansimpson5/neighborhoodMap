20eb122bc9f272a80ab58a6d5d78909a5bafd72a
Nathan Simpson <nathansimpson5@gmail.com>
2017-08-03 20:52:19 -0400

    add places and markers

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
-// TODO: make list of places
 // TODO: iterate over knockout array to add items on left
 // TODO: add info to infowindow
 
+var map;
+
+var markers = [];
 
 
 function initMap() {
-	var midtown = {lat: 33.7861132, lng: -84.3896419};
 
 	//make map and center it on Atlantic House
-	var map = new google.maps.Map(document.getElementById('map'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 15,
-	  center: midtown
+	  center: {lat: 33.7861132, lng: -84.3896419}
 	});
 
-	// Create marker for midtown
-	var marker = new google.maps.Marker({
-	  position: midtown,
-	  map: map
-	});
+	var places = [
+		{name: "Arts Center Station", location: {lat: 33.789304, lng: -84.3891965}},
+		{name: "DaVinci's Pizza", location: {lat: 33.788992, lng: -84.3886788}},
+		{name: "Foxtrot", location: {lat: 33.7857445, lng: -84.38633}},
+		{name: "Eleventh Street Pub", location: {lat: 33.7837559, lng: -84.3866712}},
+		{name: "Dancing Goats", location: {lat: 33.7811075, lng: -84.3867295}},
+];
 
+
+	for (var i=0; i < places.length; i++){
+		var position = places[i].location;
+		var title = places[i].name;
+
+		var marker = new google.maps.Marker({
+			map: map,
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			id: 1
+		});
+
+		markers.push(marker);
+	}
+/*
 	// Click handler to make infowindow appear
 	marker.addListener('click', function() {
 		infowindow.open(map,marker);
 	});
-
+*/
 	// Add infowindow
 	var infowindow = new google.maps.InfoWindow({
 		content: "Nathan House"
@@ -33,11 +52,6 @@ function initMap() {
 
 };
 
-//knockoutJS stuff
-var places = ko.observableArray([
-	{name: "Nathan", age: 28},
-	{name: "snake", age: 18}
-]);
 
-
-ko.applyBindings(places);
+/*
+ko.applyBindings(places);*/
