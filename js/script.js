97c0bfc737152450402dc62145a03acd6ce3a03e
Nathan Simpson <nathansimpson5@gmail.com>
2017-08-03 21:07:26 -0400

    add infowindow and click listeners

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ var markers = [];
 
 function initMap() {
 
-	//make map and center it on Atlantic House
+	// make map and center it on Atlantic House
 	map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 15,
 	  center: {lat: 33.7861132, lng: -84.3896419}
 	});
 
+	// make list of places
 	var places = [
 		{name: "Arts Center Station", location: {lat: 33.789304, lng: -84.3891965}},
 		{name: "DaVinci's Pizza", location: {lat: 33.788992, lng: -84.3886788}},
@@ -22,7 +23,9 @@ function initMap() {
 		{name: "Dancing Goats", location: {lat: 33.7811075, lng: -84.3867295}},
 ];
 
+	var largeInfoWindow = new google.maps.InfoWindow();
 
+	// iterate over list and make marker for each place
 	for (var i=0; i < places.length; i++){
 		var position = places[i].location;
 		var title = places[i].name;
@@ -36,17 +39,30 @@ function initMap() {
 		});
 
 		markers.push(marker);
+
+		//add click even for infowindow
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfoWindow);
+		})
+
+		// add infowindow
+		var infowindow = new google.maps.InfoWindow({
+			content: title
+		});
 	}
-/*
-	// Click handler to make infowindow appear
-	marker.addListener('click', function() {
-		infowindow.open(map,marker);
-	});
-*/
-	// Add infowindow
-	var infowindow = new google.maps.InfoWindow({
-		content: "Nathan House"
-	});
+
+// function to add content to infowindow
+function populateInfoWindow(marker, infowindow) {
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+
+		infowindow.addListener('closeclick', function(){
+			infowindow.setMarker = null;
+		});
+	}
+}
 
 
 
